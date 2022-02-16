4f6a13fb10de5f10f11ef5c187cdbfaaae52ce18
Joao Chrisostomo <maturandomassas@gmail.com>
2022-02-16 02:16:36 -0300

    foi acrescentado uma frase de programador e efeito responsivo

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-var i=0;
-function expand(){
-  if(i==0){
-document.getElementById("menu").style.transform="scale(3)"; 
-   document.getElementById("plus").style.transform="rotate(45deg)"; 
-    i=1;
-  }
-  else{   document.getElementById("menu").style.transform="scale(0)"; 
-   document.getElementById("plus").style.transform="rotate(0deg)"; 
-    i=0;
-  }
-}
+// var i=0;
+// function expand(){
+//   if(i==0){
+// document.getElementById("menu").style.transform="scale(3)"; 
+//    document.getElementById("plus").style.transform="rotate(45deg)"; 
+//     i=1;
+//   }
+//   else{   document.getElementById("menu").style.transform="scale(0)"; 
+//    document.getElementById("plus").style.transform="rotate(0deg)"; 
+//     i=0;
+//   }
+// }
+
+// const cursor = document.querySelector('.cursor');
+// document.addEventListener('mousemove', (e) => {
+//   cursor.style.left = e.pageX + 'px';
+//   cursor.style.top = e.pageY + 'px';
+// })
