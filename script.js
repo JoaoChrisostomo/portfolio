1f9c46eda448e349c0bd24d473e7cc9541fd5eaf
Joao Chrisostomo <maturandomassas@gmail.com>
2022-02-20 04:15:24 -0300

    formulario pra contato no rodape esta sendo feito

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// var i=0;
-// function expand(){
-//   if(i==0){
-// document.getElementById("menu").style.transform="scale(3)"; 
-//    document.getElementById("plus").style.transform="rotate(45deg)"; 
-//     i=1;
-//   }
-//   else{   document.getElementById("menu").style.transform="scale(0)"; 
-//    document.getElementById("plus").style.transform="rotate(0deg)"; 
-//     i=0;
-//   }
-// }
 
-// const cursor = document.querySelector('.cursor');
-// document.addEventListener('mousemove', (e) => {
-//   cursor.style.left = e.pageX + 'px';
-//   cursor.style.top = e.pageY + 'px';
-// })
+
+// alteração de idiomas
+
+// $function() {
+//     $(".br").click(function() {
+//         // cabeçalho
+//         $(".navbar").children().eq(0).text("ABOUT-ME");
+//         $(".navbar").children().eq(1).text("PROJECT");
+//         $(".navbar").children().eq(2).text("SKILLS");
+//         $(".navbar").children().eq(3).text("SERVICES");
+//         $(".navbar").children().eq(3).text("CONTACT");
+//         $(".language-selected").text("PT-BR");
+//         $(".language-selected").addClass(".change-br");
+
+    
+//     })
+// }
